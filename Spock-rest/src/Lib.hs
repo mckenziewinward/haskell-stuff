bf9ab60be2f82d0c6af1dc626b0e7576d38493b2
McKenzie Winward <mckenziewinward@gmail.com>
2019-10-21 14:26:27 -0600

    Removed unnecessary imports and Right Strings

--- Spock-rest/src/Lib.hs
+++ Spock-rest/src/Lib.hs
@@ -3,18 +3,13 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Lib (handler) where
 
-import Web.Spock hiding (body, head)
-import Web.Spock.Config
 import Text.RE.TDFA.String ((?=~), re, matched) 
 import Text.Read (readMaybe)
 import Text.Printf (printf)
 import Data.Char (isUpper)
-import Data.Either
-import Data.Aeson hiding (json)
-import Data.Aeson.Types
+import Data.Aeson
 import GHC.Generics
 import qualified Data.Text as T
-import qualified Data.ByteString.Lazy.Char8 as LBStr
 import qualified Data.ByteString.Char8 as BStr
 
 data EmployeeModel = EmployeeModel
@@ -30,51 +25,51 @@ handler body =
     case eitherDecodeStrict body of
         Right EmployeeModel { name = n, age = a, emailAddress = e } ->
             case verifyAndCreateEmployee n a e of
-                Right emp -> Right $ T.pack ("Success: " ++ n ++ " verified")
+                Right _   -> Right $ T.pack ("Success: " ++ n ++ " verified")
                 Left eMsg -> Left $ T.pack eMsg 
-        Left e -> Left $ T.pack e 
+        Left parseError -> Left $ T.pack parseError
 
 
-verifyAndCreateEmployee :: String -> String -> String -> Either String EmployeeModel
+verifyAndCreateEmployee :: String -> String -> String -> Either String ()
 verifyAndCreateEmployee name age emailAddress = 
     case verified of 
-        Right _  -> Right $ EmployeeModel { name = name, age = age, emailAddress = emailAddress }
+        Right _  -> Right () 
         Left err -> Left err
     where verified = verifyName name >> verifyAge age >> verifyEmailAddress emailAddress
 
-verifyName :: String -> Either String String
+verifyName :: String -> Either String () 
 verifyName ""   = Left "Name cannot be empty"
-verifyName name = checkNameLength name >>= isNameCapitalized
+verifyName name = checkNameLength name >> isNameCapitalized name
 
-checkNameLength :: String -> Either String String
+checkNameLength :: String -> Either String ()
 checkNameLength name 
     | length name > 20 = Left $ printf "Length of %s is not 4" name
-    | otherwise        = Right name
+    | otherwise        = Right ()
 
-isNameCapitalized :: String -> Either String String
+isNameCapitalized :: String -> Either String ()
 isNameCapitalized name 
-    | isUpper (head name) = Right name
+    | isUpper (head name) = Right ()
     | otherwise           = Left "Name should be capitalized"
 
-verifyAge :: String -> Either String String
+verifyAge :: String -> Either String () 
 verifyAge ""  = Left "Age cannot be empty"
-verifyAge age = isAgeNumber age >>= isAgeNegative
+verifyAge age = isAgeNumber age >> isAgeNegative age
 
-isAgeNumber :: String -> Either String String
+isAgeNumber :: String -> Either String ()
 isAgeNumber age = 
     case (readMaybe age :: Maybe Int) of 
-        Just _  -> Right age
+        Just _  -> Right ()
         Nothing -> Left "Age should be a number"
 
-isAgeNegative :: String -> Either String String
+isAgeNegative :: String -> Either String ()
 isAgeNegative age 
     | a < 0     = Left "Age should be greater than 0"
-    | otherwise = Right age 
+    | otherwise = Right ()
     where a = read age
 
-verifyEmailAddress :: String -> Either String String
+verifyEmailAddress :: String -> Either String ()
 verifyEmailAddress "" = Left "Email address cannot be empty"
 verifyEmailAddress emailAddress  
-    | matched $ emailAddress ?=~ regex = Right emailAddress
+    | matched $ emailAddress ?=~ regex = Right ()
     | otherwise = Left $ printf "Invalid email address: %s" emailAddress
     where regex = [re|[a-zA-Z0-9_]+([.-]?[a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.-]?[a-zA-Z0-9_]+)*(\.[a-zA-Z0-9_]{2,3})+|]
